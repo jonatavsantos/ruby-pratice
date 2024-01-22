7cf1ac82d1cd1489c45510ad985714f5a6660672
Jonata Venânico <118224160+jonatavsantos@users.noreply.github.com>
2024-01-22 19:17:37 +0000

    The expo.rb file carries out a method that receives two numbers and displays the result of raising one to the other

---
+++ method/expo.rb
@@ -0,0 +1,22 @@
+loop do
+print "Para continuar digite 1. Para encerrar digite 2: "
+
+continue = gets.chomp.to_i
+
+case continue
+when 1
+    print "Digite um número base: "
+    base = gets.chomp.to_i
+    print "Digite seu expoente: "
+    expoente = gets.chomp.to_i
+
+    def response(base, expoente)
+        calc = base ** expoente
+    end
+
+puts response(base, expoente)
+
+when 2
+    break
+end
+end
